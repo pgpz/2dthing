d5b2601ce2d29495d7d0785e34bc78cb9b7402d3
pgp <admin@pgpkey.net>
2026-01-07 01:03:57 -0600

    2d init

--- Main.c
+++ Main.c
@@ -1,4 +1,7 @@
 #include "raylib.h"
+#include <math.h>
+
+#define MAX_BUILDINGS   100
 
 int main(void)
 {
@@ -6,6 +9,16 @@ int main(void)
   const int screenHeight = 1080;
 
   InitWindow(screenWidth, screenHeight, "pokeidk");
+
+  Rectangle player = { 400, 280, 40, 40 };
+
+  int spacing = 0;
+
+  Camera2D camera = { 0 };
+  camera.target = (Vector2){ player.x + 20.0f, player.y + 20.0f };
+  camera.offset = (Vector2){ screenWidth/2.0f, screenHeight/2.0f };
+  camera.rotation = 0.0f;
+  camera.zoom = 1.0f;
 
   SetTargetFPS(120);
 
